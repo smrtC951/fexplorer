803c4168eba19718e84615da2b992908293eb96f
smrtC951 <smrtC951@gmail.com>
2025-09-24 16:41:40 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const addressBar = document.getElementById('addressBar');
+const goButton = document.getElementById('goButton');
+const browserContent = document.getElementById('browserContent');
+const backButton = document.getElementById('backButton');
+const homeButton = document.getElementById('homeButton');
+const fpointsCounter = document.getElementById('fpointsCounter');
+
+const HOME_URL = 'fexplorer:home';
